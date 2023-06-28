3a335c738ea94fe9614dbee5095f6056a822a40e
Victor Wekesa <wekae.dev@gmail.com>
2023-06-28 03:13:21 +0300

    register searchHandler for /search url pattern

--- main.go
+++ main.go
@@ -50,6 +50,8 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
