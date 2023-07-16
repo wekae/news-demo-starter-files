3327fa86752bf448545ed525ea87da46ffc2e6b9
Victor Wekesa <wekae.dev@gmail.com>
2023-07-17 01:06:54 +0300

    format published date

--- news/news.go
+++ news/news.go
@@ -67,6 +67,14 @@ type Article struct {
 	Content     string    `json:"content"`
 }
 
+/*
+* Add a method to the Article struct and use that to format the date instead of using the default value
+ */
+func (a *Article) FormatPublishedDate() string {
+	year, month, day := a.PublishedAt.Date()
+	return fmt.Sprintf("%v, %d, %d", month, day, year)
+}
+
 type Results struct {
 	Status       string    `json:"status"`
 	TotalResults int       `json:"totalResults"`
